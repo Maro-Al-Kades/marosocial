26a2e62d0e56ff5fdc4013ad42f049f0b193e07e
Maro-Al-Kades <marooasam1@gmail.com>
2024-09-10 00:05:37 +0300

    second

---
+++ app/loading.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Grid } from "react-loader-spinner";
+
+const loading = () => {
+  return (
+    <div className="">
+      <div className="h-screen w-full items-center justify-center flex mx-auto">
+        <Grid
+          visible={true}
+          height="50"
+          width="50"
+          color="#e11d48"
+          ariaLabel="grid-loading"
+          radius="12.5"
+          wrapperStyle={{}}
+          wrapperClass="grid-wrapper"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default loading;
